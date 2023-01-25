05574c5def606665f5c62b0fb557c1d487539c8f
jalebi <mehrdad.jalebi@gmail.com>
2023-01-25 19:52:31 +0330

    feat: add style

--- src/components/CharacterItem.vue
+++ src/components/CharacterItem.vue
@@ -1,18 +1,28 @@
+<script setup>
+defineProps({
+  character: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
 <template>
   <div class="item">
+    <h3>{{ character.name }}</h3>
   </div>
 </template>
 
 <style scoped>
 .item {
-  margin-top: 2rem;
+  margin: 1rem auto;
   display: flex;
+  border: 1px solid var(--color-text);
+  border-radius: 1rem;
+  padding: 1.5rem;
+  width: 70vw;
 }
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
 
 i {
   display: flex;
@@ -26,51 +36,6 @@ i {
 h3 {
   font-size: 1.2rem;
   font-weight: 500;
-  margin-bottom: 0.4rem;
   color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: " ";
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: " ";
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
 }
 </style>
--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,5 +1,5 @@
 <script setup>
-//import CharacterItem from "./CharacterItem.vue";
+import CharacterItem from "./CharacterItem.vue";
 import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
 
@@ -19,11 +19,21 @@ const { result, loading, error } = useQuery(CHARACTERS_QUERY);
 </script>
 
 <template>
-  <!--<CharacterItem />-->
-  <p v-if="error">Something went wrong...</p>
-  <p v-if="loading">Loading...</p>
-  <p v-else v-for="character in result.characters.results" :key="character.id">
-    {{ character.name }}
-  </p>
-  <div></div>
+  <div class="list">
+    <p v-if="error">Something went wrong...</p>
+    <img src="@/assets/loading.svg" v-if="loading" />
+    <p v-else v-for="character in result.characters.results" :key="character.id">
+    <CharacterItem :character="character" />
+    </p>
+  </div>
 </template>
+<style scoped>
+.list {
+  margin: 3rem 6rem;
+  display: flex;
+  padding: 1rem;
+  align-items: center;
+  flex-flow: column;
+  height: 80vh;
+}
+</style>
