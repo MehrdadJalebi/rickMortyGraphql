6d5ade72f23e40c621bbe8598588afe1df1256be
jalebi <mehrdad.jalebi@gmail.com>
2023-02-04 13:54:55 +0330

    feat: infinite scroll - step 1

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -1,11 +1,14 @@
 <script setup>
+import {computed} from "vue";
 import CharacterItem from "./CharacterItem.vue";
 import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
 
 const CHARACTERS_QUERY = gql`
-  query Characters {
-    characters {
+  query Characters ($page: Int) {
+    characters (
+      page: $page
+    ) {
       results {
         id
         name
@@ -14,17 +17,41 @@ const CHARACTERS_QUERY = gql`
     }
   }
 `
-
-const { result, loading, error } = useQuery(CHARACTERS_QUERY);
+const { result, loading, error, fetchMore } = useQuery(CHARACTERS_QUERY, { variables: { page: 1 }});
+const characters = computed(() => {
+  return result.value?.characters.results
+});
+const scrollEnd = () => fetchMore({
+  variables: {
+    page: characters.length / 20 + 1
+  },
+  updateQuery: (prev, { fetchMoreResult }) => {
+    if (!fetchMoreResult) return prev;
+    return {
+      characters: {
+        __typename: 'Characters',
+        results: [
+          ...prev.characters.results,
+          ...fetchMoreResult.characters.results
+        ]
+      }
+    }
+  }
+})
 </script>
 
 <template>
   <div class="list">
     <p v-if="error">Something went wrong...</p>
     <img src="@/assets/loading.svg" v-if="loading" />
-    <p v-else v-for="character in result.characters.results" :key="character.id">
+    <p v-else v-for="character in characters" :key="character.id">
     <CharacterItem :character="character" />
     </p>
+    <button
+      @click="scrollEnd"
+    >
+      Load More
+    </button>
   </div>
 </template>
 <style scoped>
